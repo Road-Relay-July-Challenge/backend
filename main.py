73b4afc57a5081046f00c03c7073ffe7b666db55
Ho Zhen Hong <hozhenhong99@gmail.com>
2022-06-29 13:29:08 +0800

    add account details

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from flask import Flask
 from auth import auth_api
 from config import DEBUG
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
 app = Flask(__name__)
 app.register_blueprint(auth_api, url_prefix='/auth')
 
