74d5b25b3d286e1cdcf55414c3ef4cb791a51762
Jason Ng <jasonng@iappsasia.com>
2022-06-29 15:51:44 +0800

    Auth

--- server/auth.py
+++ server/auth.py
@@ -3,28 +3,36 @@ import requests
 import urllib3
 from config import CLIENT_ID, CLIENT_SECRET
 from routes import VERIFY, OAUTH_URL, REFRESH_ALL
+from utils import return_json
 from db import add_person
 
 auth_api = Blueprint('auth_api', __name__)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disables insecure request warning for login
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disables insecure request warning for verify
 
 @auth_api.route(VERIFY)
-def login():
+def verify():
     args = request.args
     authorizationCode = args.get('code')
-    print("Authorization code:", authorizationCode)
-    print("Client ID:", CLIENT_ID)
     payload = {
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
         'code': authorizationCode,
         'grant_type': 'authorization_code'
     }
-    response = requests.post(OAUTH_URL, data=payload, verify=False)
+    response = requests.post(OAUTH_URL, data=payload, verify=False).json()
 
-    # add response.refresh_token, response.athlete.username, response.athlete.id into DB)
+    person = {
+        "name": response.get('athlete').get('firstname') + " " + response.get('athlete').get('lastname'),
+        "athlete id": response.get('athlete').get('id'),
+        "access token": response.get('access_token'),
+        "access token expired at": response.get('expires_at'),
+        "refresh token": response.get("refresh_token"),
+        "mileage": 0
+    }
 
-    return response.json()
+    add_person(person)
+    print(f"Successfully added {person['name']}")
+    return return_json(True, f"Successfully added {person['name']}")
 
 @auth_api.route(REFRESH_ALL)
 def refresh_all():
--- server/utils.py
+++ server/utils.py
@@ -1,7 +1,14 @@
 import requests
 import datetime
+from flask import jsonify
 from config import CLIENT_ID, CLIENT_SECRET
 from routes import OAUTH_URL
+
+def return_json(isSuccess, returnMessage):
+    return jsonify(
+        success=isSuccess,
+        message=returnMessage
+    )
 
 def get_new_access_token(refresh_token):
     payload = {
