3dee4a6c4addca41ecd3b1e3687abbe7000b76fd
Ho Zhen Hong <hozhenhong99@gmail.com>
2022-06-29 13:34:47 +0800

    fix some error

--- server/app.py
+++ server/app.py
@@ -3,12 +3,9 @@ from flask import Flask
 from auth import auth_api
 from config import DEBUG
 
-<<<<<<< HEAD:main.py
-=======
 from firebase_admin import credentials
 from firebase_admin import firestore
 
->>>>>>> 6c7217533ac0f122ba65c5fdaa1ef802eb62d450:server/app.py
 app = Flask(__name__)
 app.register_blueprint(auth_api, url_prefix='/auth')
 
