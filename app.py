4584aa9b45d9bbcec01005bff9abc3f7e91836ff
Jason Ng <54024886+jason-ng-zq99@users.noreply.github.com>
2022-07-02 15:16:00 +0800

    Clean up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
+# from flask_restful import Api, Resource, reqparse
 from server.auth import auth_api
 from server.team import team_api
 from server.individual import individual_api
@@ -9,11 +9,8 @@ app = Flask(__name__, static_url_path='', static_folder='client/build')
 app.register_blueprint(auth_api, url_prefix='/auth')
 app.register_blueprint(team_api, url_prefix='/team')
 app.register_blueprint(individual_api, url_prefix='/individual')
-api = Api(app)
+# api = Api(app)
 
-# @app.route("/")
-# def hello():
-#     return "Hello world"
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
