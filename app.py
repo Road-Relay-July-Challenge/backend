0133d9c831548df89817363b4229ef19b47257e6
Jason Ng <54024886+jason-ng-zq99@users.noreply.github.com>
2022-07-02 14:58:28 +0800

    Edit app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from server.auth import auth_api
 from server.team import team_api
 from server.individual import individual_api
@@ -9,9 +9,12 @@ app.register_blueprint(auth_api, url_prefix='/auth')
 app.register_blueprint(team_api, url_prefix='/team')
 app.register_blueprint(individual_api, url_prefix='/individual')
 
-@app.route("/")
-def hello():
-    return "Hello world"
+# @app.route("/")
+# def hello():
+#     return "Hello world"
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
 if __name__ == "__main__":
     app.run(debug=DEBUG)
