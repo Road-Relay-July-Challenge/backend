4acff5f473c99d0127305ff011d94bef96a84f8a
Jason Ng <54024886+jason-ng-zq99@users.noreply.github.com>
2022-07-03 12:45:35 +0800

    Enable CORS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api
+from flask_cors import CORS
 from server.auth import auth_api
 from server.team import team_api
 from server.individual import individual_api
 from server.config import DEBUG
 
 app = Flask(__name__, static_url_path='', static_folder='client/build')
+CORS(app)
 app.register_blueprint(auth_api, url_prefix='/auth')
 app.register_blueprint(team_api, url_prefix='/team')
 app.register_blueprint(individual_api, url_prefix='/individual')
