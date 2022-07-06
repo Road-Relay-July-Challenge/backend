e9cb51027e463bd8378d84fa4f99cbb0f9aab224
Jason Ng <jasonng@iappsasia.com>
2022-07-06 17:05:07 +0800

    Update scheduler to include east west job

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,9 @@
-from server.auth import refresh_all
+from server.auth import refresh_all, refresh_all_east_west
 from server.db import update_refresh_time
 from app import app
 
 if __name__ == "__main__":
     with app.app_context():
         refresh_all()
+        refresh_all_east_west()
         update_refresh_time()
