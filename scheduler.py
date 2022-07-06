9b8309f6e95a1e268987079de84756ca57090a51
Jason Ng <jasonng@iappsasia.com>
2022-07-06 09:44:19 +0800

    Add refresh time during scheduled refreshes

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,8 @@
 from server.auth import refresh_all
+from server.db import update_refresh_time
 from app import app
 
 if __name__ == "__main__":
     with app.app_context():
         refresh_all()
+        update_refresh_time()
